65b44b3e8d6cfabcec812342862400854529a711
gavinclark336 <36719399+gavinclark336@users.noreply.github.com>
2018-03-20 04:27:52 +0000

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,2 @@
+    const _ = require('lodash');const utils = require('../utils'); // Source: https://coinmarketcap.com/apiconst cryptoAssetList = [ '0x', 'bitcoin', // Bitcoin 'bitcoin-cash', // Bitcoin cash 'ethereum', // Ethereum
+    
